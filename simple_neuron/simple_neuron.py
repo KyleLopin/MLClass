fe79cd88d1bd580810f81568dd4a900fdbbf60a0
Kyle Lopin <kylelopin@gmail.com>
2025-05-22 11:55:41 +0700

    make Lesson 1 and refactored to make it easier to teach

--- simple_neuron/simple_neuron.py
+++ simple_neuron/simple_neuron.py
@@ -10,21 +10,29 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+
 class SimpleNeuron:
     def __init__(self, number_inputs: int, number_outputs: int):
         """
         Initialize a simple neuron model with given number of inputs and outputs.
         """
+        self.number_inputs = number_inputs
         self.model = nn.Linear(number_inputs, number_outputs)
         self.loss_fn = nn.MSELoss()
         self.optimizer = optim.SGD(self.model.parameters(), lr=0.01)
 
-    def fit(self, input_data: list, output_data: list, epochs: int = 1000):
+    def train(self, input_data: list, output_target: list, epochs: int = 1000):
         """
         Fit the neuron to the input-output data.
+        Automatically reshapes 1D input/output lists to 2D tensors.
         """
+        if isinstance(input_data[0], (int, float)) and self.number_inputs == 1:
+            input_data = [[val] for val in input_data]
+        if isinstance(output_target[0], (int, float)) and self.model.out_features == 1:
+            output_target = [[val] for val in output_target]
+
         x = torch.tensor(input_data, dtype=torch.float32)
-        y = torch.tensor(output_data, dtype=torch.float32)
+        y = torch.tensor(output_target, dtype=torch.float32)
 
         for _ in range(epochs):
             y_pred = self.model(x)
@@ -60,9 +68,39 @@ class SimpleNeuron:
     def get_output(self, x: list):
         """
         Predict output for a new input.
+        Accepts:
+        - A single input sample as a flat list if number_inputs > 1
+        - A list of numbers if number_inputs == 1 (will be reshaped)
+        - A list of samples (list of lists)
+        Returns:
+        - A list of outputs if number_inputs == 1
+        - A list of list outputs otherwise
         """
+        is_single_input = isinstance(x[0], (int, float))
+
+        if is_single_input:
+            if self.number_inputs == 1:
+                x = [[val] for val in x]  # reshape
+            else:
+                x = [x]  # one sample with multiple inputs
+
         x_tensor = torch.tensor(x, dtype=torch.float32)
         with torch.no_grad():
-            output = self.model(x_tensor)
-        return output.numpy()
+            output = self.model(x_tensor).numpy()
 
+        if self.number_inputs == 1 and is_single_input:
+            return output.flatten().tolist()
+        return output.tolist()
+
+
+if __name__ == '__main__':
+    # lesson 1
+    neuron = SimpleNeuron(number_inputs=1, number_outputs=1)
+    neuron.set_weights([1])
+    neuron.set_bias([1])
+    output = neuron.get_output([0, 1, 2, 3])
+    print(output)
+    neuron.properties()
+    # lesson 2:
+    neuron.train(input_data=[0, 1, 2, 3], output_target=[2, 4, 6, 8])
+    neuron.properties()
